406dad4b3b84cf9ca5524637ba37072657231379
KangYeSeo04 <e64888283@gmail.com>
2026-03-15 16:09:15 +0900

    Boj/silver/10845 (#363)
    * boj/Bronze/14467
    
    * solved boj/Bronze/21918
    
    * solved bog/Silver/20546
    
    * solved boj/Silver/7568
    
    * solved boj/Silver/10709
    
    * sovled boj/Silver/9017
    
    * solved soj/Silver/2852
    
    * solved boj/Silver/1913
    
    * solved boj/Silver/20006
    
    * solved soj/Silver/1138
    
    * solved boj/Silver/17276
    
    * solved boj/Silver/16926
    
    * solved boj/Gold/14719
    
    * solved boj/Gold/20207
    
    * solved boj/Gold/22860
    
    * solved boj/Silver/10828
    
    * solved boj/Silver/10845

---
+++ kangyeseo0724/BaekJoon/Silver/10845큐.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+def solve():
+    n = int(input())
+    queue = deque()
+    
+    for _ in range(n):
+        command = input().split()
+        
+        if command[0] == 'push':
+            queue.append(command[1])
+        elif command[0] == 'pop':
+            if not queue:
+                print(-1)
+            else:
+                print(queue.popleft())
+        elif command[0] == 'size':
+            print(len(queue))
+        elif command[0] == 'empty':
+            print(1 if not queue else 0)
+        elif command[0] == 'front':
+            if not queue:
+                print(-1)
+            else:
+                print(queue[0])
+        elif command[0] == 'back':
+            if not queue:
+                print(-1)
+            else:
+                print(queue[-1])
+
+solve()
