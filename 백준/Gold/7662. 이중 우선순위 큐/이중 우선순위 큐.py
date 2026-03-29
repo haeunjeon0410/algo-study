b1979b2a31a72d52307bf84cf0d41a1cd2014d72
haeunjeon0410 <janice@sookmyung.ac.kr>
2026-03-30 08:40:24 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2908 ms, Memory: 353152 KB -BaekjoonHub

---
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -0,0 +1,45 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    min_h = []
+    max_h = []
+    visit = [False] * k
+
+    for i in range(k):
+
+        cmd, n = input().split()
+        
+        if cmd == 'I':
+            heapq.heappush(min_h, (int(n), i))
+            heapq.heappush(max_h, (-int(n), i))
+            visit[i] = True
+
+        elif cmd == 'D':
+            if n == '1':
+                while max_h and not visit[max_h[0][1]]:
+                    heapq.heappop(max_h)
+                if max_h:
+                    _, idx = heapq.heappop(max_h)
+                    visit[idx] = False
+            else:
+                while min_h and not visit[min_h[0][1]]:
+                    heapq.heappop(min_h)
+                if min_h:
+                    _, idx = heapq.heappop(min_h)
+                    visit[idx] = False
+        
+    while min_h and not visit[min_h[0][1]]:
+        heapq.heappop(min_h)
+
+    while max_h and not visit[max_h[0][1]]:
+        heapq.heappop(max_h)
+
+    if not min_h:
+        print('EMPTY')
+    else:
+        print(-max_h[0][0], min_h[0][0])
